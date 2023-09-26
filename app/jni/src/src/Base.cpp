a62f08349d209e2d006eb24e5e694466b6629116
gittem01 <mchtt43@gmail.com>
2023-09-26 21:07:48 +0300

    Vulkan instance creation

--- app/jni/src/src/Base.cpp
+++ app/jni/src/src/Base.cpp
@@ -1,6 +1,7 @@
 #include <Base.h>
 #include <DebugRenderer.h>
 #include <set>
+#include <vulkan/vulkan.h>
 
 void Base::createTestBodies()
 {
@@ -90,14 +91,33 @@ public:
 
 Base::Base()
 {
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    createInfo.pApplicationInfo = &appInfo;
+
+    VkInstance instance;
+    VkResult res = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if (res != VK_SUCCESS)
+    {
+        exit(1);
+    }
+
     memset(fingerPresses, 0, sizeof(fingerPresses));
     memset(fingerPositions, 0, sizeof(fingerPositions));
 
     SDL_DisplayMode displayMode;
-    bool res = SDL_GetDisplayMode(0, 0, &displayMode);
+    bool r = SDL_GetDisplayMode(0, 0, &displayMode);
 
     int refreshRate;
-    if (displayMode.refresh_rate > 0 && res == 0)
+    if (displayMode.refresh_rate > 0 && r == 0)
     {
         refreshRate = displayMode.refresh_rate;
     }
